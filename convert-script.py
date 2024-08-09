7c173b7bc574632665aeb4a86599d92f2561ae3b
skyjo3 <skyjo3@gmail.com>
2024-08-08 18:41:00 -0700

    adding input/output path to the script

--- convert-script.py
+++ convert-script.py
@@ -1,15 +1,25 @@
 # convert-script.py
 
 import coremltools as ct
+import os
 
-caffe_model = ('oxford102.caffemodel', 'deploy.prototxt')
+input_dir = 'input'
+output_dir = 'output'
 
-labels = 'flower-labels.txt'
+caffe_model = (
+    os.path.join(input_dir, 'oxford102.caffemodel'),
+    os.path.join(input_dir, 'deploy.prototxt')
+)
+labels = os.path.join(input_dir, 'flower-labels.txt')
 
+# Convert the Caffe model to a CoreML model
 coreml_model = ct.converters.caffe.convert(
-	caffe_model,
-	class_labels=labels,
-	image_input_names='data'
+    caffe_model,
+    class_labels=labels,
+    image_input_names='data'
 )
 
-coreml_model.save('FlowerClassifier.mlmodel')
+output_path = os.path.join(output_dir, 'FlowerClassifier.mlmodel')
+coreml_model.save(output_path)
+
+print(f"Model saved to {output_path}")
